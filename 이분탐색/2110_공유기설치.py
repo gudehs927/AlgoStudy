cd2f21687c406cf1560261af1b921e72edf24a4a
gudehs927 <gudehs927@naver.com>
2022-09-21 21:33:31 +0900

    2022_09_21

---
+++ 이분탐색/2110_공유기설치.py
@@ -0,0 +1,28 @@
+import sys
+
+n, m = map(int, input().split())
+k = []
+for i in range(n):
+    x = int(sys.stdin.readline())
+    k.append(x)
+k.sort()
+
+start = 1
+end = k[-1] - k[0]
+root = 0
+while start <= end:
+    mid = (start + end)//2
+    sp = k[0]
+    count = 1
+
+    for i in range(1, len(k)):
+        if k[i] >= sp + mid:
+            count += 1
+            sp = k[i]
+    if count >= m:
+        start = mid + 1
+        root = mid
+    else:
+        end = mid - 1
+
+print(root)
