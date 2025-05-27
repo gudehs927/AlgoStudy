c3de1eb9d0e076050fe19f74f5eb2d175d542cfc
gudehs927 <56256613+gudehs927@users.noreply.github.com>
2025-05-27 12:02:13 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) 
+{
+    if(0 < angle && angle <90)
+    {
+        return 1;
+    }
+    else if(angle == 90)
+    {
+        return 2;
+    }
+    else if(90 < angle && angle < 180)
+    {
+        return 3;
+    }
+    else if(angle == 180)
+    {
+        return 4;
+    }
+}
