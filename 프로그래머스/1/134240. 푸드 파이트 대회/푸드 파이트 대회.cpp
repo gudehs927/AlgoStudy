e5784c05f84bb37748cf3fed1b2e160feb2b5f87
gudehs927 <56256613+gudehs927@users.noreply.github.com>
2025-07-08 15:35:03 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.31 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -6,10 +6,10 @@ using namespace std;
 string solution(vector<int> food) {
     string answer = "";
     for(int i = 1; i < food.size(); i++){
-        for(int j =0; j < food[i]/2;j++){
+        for(int j = 0; j < food[i]/2; j++){
             answer += to_string(i);
         }
     }
-    string answer2 = string(answer.rbegin(),answer.rend());
-    return answer+'0'+answer2;
+    string answer2 = string(answer.rbegin(), answer.rend());
+    return answer + '0'+ answer2;
 }
